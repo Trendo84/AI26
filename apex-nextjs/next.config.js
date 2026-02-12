b50ba197737d8b6eb84843a3e0278f9733f89901
Trendo84 <161993680+Trendo84@users.noreply.github.com>
2026-02-12 16:33:04 +1100

    Enable static export for Netlify

--- apex-nextjs/next.config.js
+++ apex-nextjs/next.config.js
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
-  // output: "export",
+  output: "export",
+  distDir: "out",
   images: { unoptimized: true },
   trailingSlash: true,
 }
